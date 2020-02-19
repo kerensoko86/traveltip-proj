e57bafd5d3c7403fd87062f5e81f96141fa62526
kerensoko86 <keren.sokoletsky@gmail.com>
2020-02-19 15:57:00 +0200

    secrect weather key

--- .gitignore/secret.js
+++ .gitignore/secret.js
@@ -1,1 +1,3 @@
 export const MAP_KEY = 'AIzaSyDgPu4hpyj2FZsfka3H5LPwiu8HifWQvqA';
+
+export const WEATHER_KEY = '97fad777ac48a0fb3701c316fb482470';
--- travel-tip-startHere/js/services/weather.service.js
+++ travel-tip-startHere/js/services/weather.service.js
@@ -1,8 +1,9 @@
 'use strict';
 
+import { WEATHER_KEY } from './.gitignore.secret.js'
 
 export function connectWeather(lat, lon) {
-    const API_KEY = '97fad777ac48a0fb3701c316fb482470';
+    const API_KEY = WEATHER_KEY;
     var weather = axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}`)
         .then(res => {
             console.log(res, 'res');
